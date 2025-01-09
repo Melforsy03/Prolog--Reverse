ec7a66cd50df51c2b6fc7432248685489d16dd59
username <ibaezmustelier@gmail.com>
2025-01-09 10:07:11 -0500

    uno sin espacio

--- visual.pl
+++ visual.pl
@@ -72,7 +72,7 @@ reemplazar_fila([white | Resto], [2 | RestoReemplazado]) :-
 
 escribir_end(Jugador, Puntuacion) :-
     open('ireverse/end.txt', write, StreamIn),
-    write(Stream, '1 '),  % Comienza con un 1 para indicar que es una respuesta válida
+    write(Stream, '1'),  % Comienza con un 1 para indicar que es una respuesta válida
     write(Stream, Jugador),
     write(Stream, Puntuacion),
     escribir_matriz(Stream, Matriz),
